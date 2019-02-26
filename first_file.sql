9c88baaa3c8e3e03db9b6a403893b543d5086c7c
Dennis Daubney Jr <dennis.daubney@gmail.com>
2019-02-26 18:11:39 +0000

    added testing folder to test SQL commands from external files including table creation and insertion

---
+++ first_file.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cats_test1
+    (
+        cat_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100),
+        AGE INT
+        );
+        
---
+++ testing/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats_test1(name, AGE)
+VALUES('Charlie', 17);
+
+INSERT INTO cats_test1(name, AGE)
+VALUES('Connie', 10);
+
+SELECT * FROM cats_test1;
